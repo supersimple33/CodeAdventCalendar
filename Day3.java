206ca6617e605372b5fd90ea1fed0db1f4cb0a59
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2020-12-03 17:16:37 -0500

    Completed Day 3 Part 2

--- Day3.java
+++ Day3.java
@@ -15,5 +15,48 @@ public class Day3{
 			position += 3;
 		}
 		System.out.println(counter);
+
+		// Slope for 1,-1
+		int counterB = 0;
+		reader = new Scanner(supp);
+		position = 0;
+		while (reader.hasNextLine()) {
+			if (reader.nextLine().charAt(position % 31) == '#') {
+				counterB++;
+			}
+			position += 1;
+		}
+		// Slope for 5,-1
+		int counterC = 0;
+		reader = new Scanner(supp);
+		position = 0;
+		while (reader.hasNextLine()) {
+			if (reader.nextLine().charAt(position % 31) == '#') {
+				counterC++;
+			}
+			position += 5;
+		}
+		// Slope for 7,-1
+		int counterD = 0;
+		reader = new Scanner(supp);
+		position = 0;
+		while (reader.hasNextLine()) {
+			if (reader.nextLine().charAt(position % 31) == '#') {
+				counterD++;
+			}
+			position += 7;
+		}
+		// Slope for 1,-2
+		int counterE = 0;
+		reader = new Scanner(supp);
+		position = 0;
+		while (reader.hasNextLine()) {
+			if (reader.nextLine().charAt(position % 31) == '#') {
+				counterE++;
+			}
+			position += 1;
+			if (reader.hasNextLine()) reader.nextLine();
+		}
+		System.out.println(counter * counterB * counterC * counterD *counterE);
 	}
 }
