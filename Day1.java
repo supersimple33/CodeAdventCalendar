57acb405d714f5e61b2d74925de44f93e124347e
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2020-12-03 11:58:02 -0500

    Initial Commit

---
+++ Day1.java
@@ -0,0 +1,9 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Day1{
+	 public static void main(String []args) throws FileNotFoundException {
+		//
+	 }
+}
