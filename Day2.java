81bf72bab24246035b1473137c783b3ac24b0449
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2020-12-03 16:53:40 -0500

    Completed Day 2 Part 2

--- Day2.java
+++ Day2.java
@@ -7,14 +7,28 @@ public class Day2{
 		File supp = new File("supp2.txt");
 		Scanner reader = new Scanner(supp);
 		int counter = 0;
+		// while (reader.hasNextLine()) {
+		// 	String str = reader.nextLine();
+		// 	// System.out.println(str);
+		// 	int min = Integer.parseInt(str.split("[- :]+")[0]) + 1;
+		// 	int max = Integer.parseInt(str.split("[- :]+")[1]) + 1;
+		// 	String ch = str.split("[- :]+")[2];
+		// 	String pass = str.split("[- :]+")[3].replaceAll(".", "$0 ");
+		// 	if ((pass.split(ch).length <= max) && (pass.split(ch).length >= min)) {
+		// 		counter++;
+		// 	}
+		// }
+		// System.out.println(counter);
+
+		counter = 0;
 		while (reader.hasNextLine()) {
 			String str = reader.nextLine();
 			// System.out.println(str);
-			int min = Integer.parseInt(str.split("[- :]+")[0]) + 1;
-			int max = Integer.parseInt(str.split("[- :]+")[1]) + 1;
-			String ch = str.split("[- :]+")[2];
-			String pass = str.split("[- :]+")[3].replaceAll(".", "$0 ");
-			if ((pass.split(ch).length <= max) && (pass.split(ch).length >= min)) {
+			int min = Integer.parseInt(str.split("[- :]+")[0]) - 1;
+			int max = Integer.parseInt(str.split("[- :]+")[1]) - 1;
+			char ch = str.split("[- :]+")[2].charAt(0);
+			String pass = str.split("[- :]+")[3];
+			if ((pass.charAt(min) == ch) != (pass.charAt(max) == ch)) {
 				counter++;
 			}
 		}
