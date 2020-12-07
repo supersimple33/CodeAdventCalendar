7cf1027a29b493a6a4ec6f0f01961b3c09a0f003
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2020-12-07 11:38:57 -0500

    Completed Day 5 Part 1

---
+++ Day5.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.Scanner;
+public class Day5{
+	public static void main(String []args) throws FileNotFoundException {
+		File supp = new File("supp5.txt");
+		Scanner reader = new Scanner(supp);
+		ArrayList<Integer> seats = new ArrayList<Integer>();
+		while (reader.hasNextLine()) {
+			String nl = reader.nextLine();
+			int row = 64;
+			for (int i = 0; i < 7; i++) {
+				char c = nl.charAt(i);
+				int sign = (c == 'F') ? -1 : 1;
+				row += Math.pow(2.0, 5.0 - i) * sign;
+			}
+			int col = 4;
+			for (int i = 7; i < 10; i++) {
+				char c = nl.charAt(i);
+				int sign = (c == 'L') ? -1 : 1;
+				col += Math.pow(2.0, 8.0 - i) * sign;
+			}
+			seats.add(row * 8 + col);
+		}
+		System.out.println(Collections.max(seats));
+	}
+}
