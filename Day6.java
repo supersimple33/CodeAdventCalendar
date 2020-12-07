df6c23a87d7cf4230dd99c3373ffa660102d9535
supersimple33 <40609224+supersimple33@users.noreply.github.com>
2020-12-07 12:13:41 -0500

    Completed Day 6 Part 1

---
+++ Day6.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.util.Scanner;
+public class Day6{
+	public static void main(String []args) throws FileNotFoundException {
+		File supp = new File("supp6.txt");
+		Scanner reader = new Scanner(supp);
+		StringBuilder sb = new StringBuilder();
+		int sum = 0;
+		while (reader.hasNextLine()) {
+			String nl = reader.nextLine();
+			if (!nl.isEmpty()) {
+				sb.append(nl);
+			} else {
+				Character[] chars = new Character[sb.length()];
+				for (int i = 0; i < sb.length(); i++) {
+					chars[i] = sb.charAt(i);
+				}
+				LinkedHashSet<Character> lset = new LinkedHashSet<Character>(Arrays.asList(chars));
+				sum += lset.size();
+				sb = new StringBuilder();
+			}
+		}
+		System.out.println(sum);
+	}
+}
